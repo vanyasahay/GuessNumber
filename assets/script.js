fb41ca83171b9aa68740ce9d3ee897b20cf34cdf
vanyasahay <vanyasahay@gmail.com>
2023-03-08 22:54:28 +0000

    gussed answer code corrected

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ var output = Math.floor(Math.random()*100) + 1;
 var guessNumber = 0;
 var numberGussed = [];
 
+var firstMessage = document.getElementsById("txt1");
+var secondMessage = document.getElementsById("txt2");
+var thirdMessage = document.getElementsById("txt3");
+
 functionNumberGuessed(){
     var userCount = document.getElementsById("myNumber").value;
         if(userCount < 1 || userCount > 100){
@@ -10,5 +14,22 @@ functionNumberGuessed(){
         else{
             numberGussed.push();
             guessNumber+=1;
+        }
+
+        if(userCount < output){
+            firstMessage.textContent = "Think higher number";
+            secondMessage.textContent ="Guess Numbe is:-" + guessNumber;
+            thirdMessage.textContent ="Your number is:-" + numberGussed;
+
+        }
+        else if(userCount < output){
+            firstMessage.textContent = "Think lower number";
+            secondMessage.textContent ="Guess Number is:-" + guessNumber;
+            thirdMessage.textContent ="Your number is:-" + numberGussed; 
+        }
+        else if(userCount < output){
+            firstMessage.textContent = "You hit the Jackpot!!";
+            secondMessage.textContent ="This was the number:- " + guessNumber;
+            thirdMessage.textContent ="which you gussed in :- " + numberGussed;
         }
 }
