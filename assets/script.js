65fac6359d46feadd11ed3fdf66de21aae8b30d5
vanyasahay <vanyasahay@gmail.com>
2023-03-08 22:22:55 +0000

    javascript code added

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+functionnumberGuessed(){
+    var userCount = document.getElementsById("myNumber").value;
+        if(userCount < 1 || userCount > 100){
+            alert("Guess a number and it should be between 1 and 100 !");
+        }
+        else{
+            
+        }
+}
