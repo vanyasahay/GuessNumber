5ac9483c04b71f3b1000870f0711e65dfdfe091b
vanyasahay <vanyasahay@gmail.com>
2023-03-08 22:38:50 +0000

    Text attribute fixed

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
-functionnumberGuessed(){
+var output = Math.floor(Math.random()*100) + 1;
+var guessNumber = 0;
+var numberGussed = [];
+
+functionNumberGuessed(){
     var userCount = document.getElementsById("myNumber").value;
         if(userCount < 1 || userCount > 100){
             alert("Guess a number and it should be between 1 and 100 !");
         }
         else{
-            
+            numberGussed.push();
+            guessNumber+=1;
         }
 }
