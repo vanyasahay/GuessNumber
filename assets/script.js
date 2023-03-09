fe54b343d3d5e8aeb0c6f37bad9d73ca623574c1
vanyasahay <vanyasahay@gmail.com>
2023-03-09 01:38:32 +0000

    js code correction

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,32 @@ var firstMessage = document.getElementsById("txt1");
 var secondMessage = document.getElementsById("txt2");
 var thirdMessage = document.getElementsById("txt3");
 
-var output = Math.floor(Math.random()*25) + 1;
+var output = Math.floor(Math.random()*100) + 1;
 var guessNumber = 0;
 var numberGussed = [];
 
 function yourNumberGuessed(){
     var userCount = document.getElementById("myNumber").value;
-    if(userCount < 1 || userCount > 25){
-        alert("Guess a number and it should be between 1 and 25 !");
+    if(userCount < 1 || userCount > 100){
+        alert("Guess a number and it should be between 1 and 100 !");
     }
     else{
         numberGussed.push(userCount);
         guessNumber+= 1;
 
-        if(user_guess < output){
+        if(userCount < output){
             msg1.textContent = "Your guess is too low.";
             msg2.textContent = "No. of guesses: " + numberGussed;
             msg3.textContent = "Guessed numbers are: " +
             guessNumber;
         }
-        else if(user_guess > output){
+        else if(userCount > output){
             msg1.textContent = "Your guess is too high.";
             msg2.textContent = "No. of guesses: " + numberGussed;
             msg3.textContent = "Guessed numbers are: " +
             guessNumber;
         }
-        else if(user_guess == output){
+        else if(userCount == output){
             msg1.textContent = "Yippie You Win!!";
             msg2.textContent = "The number was: " + output;
             msg3.textContent = "You guessed it in "+ numberGussed + " guesses";
@@ -47,6 +47,6 @@ function resetButton(){
     mybutton.disabled = false;
     myNumber.value = '';
     myNumber.focus();
-    randomNumber = Math.floor(Math.random() * 25) + 1;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
 }
 mybutton.addEventListener('click',yourNumberGuessed)
